ebe8f0874134a444cf4e8df57932efaf76f2bf57
YanWQ-monad <YanWQmonad@gmail.com>
2021-10-30 11:56:38 +0800

    deps: update dependencies and some deprecated
    NPM dependency:
    - update cheerio
    - update hexo to 5.4.0
    - update hexo-renderer-pug to 2.0.0
    - replace hexo-katex(deprecated) with hexo-katexity
    
    GitHub Actions changes:
    - remove section of modifying hexo-katex
    - move `gulp default` to workflows

--- .github/generate/entrypoint.sh
+++ .github/generate/entrypoint.sh
@@ -2,8 +2,4 @@
 
 set -e
 
-sed -i 's/decodeEntities: true/decodeEntities: false/g' node_modules/hexo-katex/index.js
-sed -i 's/$(this).text()/cheerio.load($(this).html()).html({ decodeEntities: false })/g' node_modules/hexo-katex/index.js
-
-npx gulp default
 npx hexo generate
