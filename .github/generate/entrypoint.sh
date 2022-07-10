0e3d9a70376fbff0256a5221c214879e66a96462
YanWQ-monad <YanWQmonad@gmail.com>
2022-07-11 01:51:14 +0800

    chore: set last modified date from git

--- .github/generate/entrypoint.sh
+++ .github/generate/entrypoint.sh
@@ -1,5 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
+
+# set last modified date from git
+git ls-files -z | while read -d '' path; do touch -d $(git log -1 --format="@%ct" "$path") "$path"; done
 
 npx hexo generate
