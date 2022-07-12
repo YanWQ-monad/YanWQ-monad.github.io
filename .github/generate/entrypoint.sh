700fb3ca660c14168cfd831f4d3156d97e153224
YanWQ-monad <YanWQmonad@gmail.com>
2022-07-12 23:38:43 +0800

    fix: fix hexo-katexify render error on code block

--- .github/generate/entrypoint.sh
+++ .github/generate/entrypoint.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+# fix hexo-katexify render error on code block
+sed -i "5a'hexoPostRenderCodeBlock'," node_modules/hexo-katexify/dist/lib/filter.js
+sed -i "31aif (line.startsWith('<!-- katex: off -->')) return;" node_modules/hexo-katexify/dist/lib/filter.js
+
 # set last modified date from git
 git ls-files -z | while read -d '' path; do touch -d $(git log -1 --format="@%ct" "$path") "$path"; done
 
